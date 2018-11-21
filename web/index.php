f126be9c9e10ddaf9e0742e62fafd21935ae0cf7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-11-22 06:38:23 +0800

    added readme iframe in welcome page

--- web/index.php
+++ web/index.php
@@ -5,24 +5,48 @@
 </head>
 <body>
 <link rel="stylesheet" href="style/common.css">
-<div text-center>
+<style>
+    .head {
+        height: 10em;
+    }
+
+    .root {
+        display: flex;
+        align-items: flex-start;
+    }
+
+    menu, h2 {
+        width: 10em;
+    }
+
+    iframe {
+        width: calc(100vw - 15em);
+        height: calc(100vh - 12em);
+    }
+</style>
+<div text-center class="head">
     <img src="img/logo.png">
     <h1>Welcome to Pomodoro Club</h1>
 </div>
-<menu>
-    <h2>Main Menu</h2>
-    <ul>
-        <li><a href="signup.html">Signup</a></li>
-        <li><a href="login.html">Login</a></li>
-        <li><a href="search.html">Search</a></li>
-        <li><a href="README.html">ReadMe</a></li>
-    </ul>
-    <h2>Admin Menu</h2>
-    <ul>
-        <li><a href="install.php">Install</a></li>
-        <li><a href="uninstall.php">Uninstall</a></li>
-        <li><a href="reset.php">Reset</a></li>
-    </ul>
-</menu>
+<div class="root">
+    <menu>
+        <h2>Main Menu</h2>
+        <ul>
+            <li><a href="signup.html">Signup</a></li>
+            <li><a href="login.html">Login</a></li>
+            <li><a href="search.html">Search</a></li>
+            <li><a href="README.html">ReadMe</a></li>
+        </ul>
+        <h2>Admin Menu</h2>
+        <ul>
+            <li><a href="install.php">Install</a></li>
+            <li><a href="uninstall.php">Uninstall</a></li>
+            <li><a href="reset.php">Reset</a></li>
+        </ul>
+    </menu>
+    <main>
+        <iframe src="README.html"></iframe>
+    </main>
+</div>
 </body>
 </html>
